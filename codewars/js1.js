0356331b9756c8412c141e0264e4bf9db5bbb3c9
panthawk <bambulageprashant@gmail.com>
2021-03-03 00:57:08 +0530

    first question

---
+++ codewars/js1.js
@@ -0,0 +1,13 @@
+//It's pretty straightforward. Your goal is to create a function that removes the first and
+// last characters of a string. You're given one parameter, the original string. You don'
+//t have to worry with strings with less than two characters.
+function removeChar(str){
+ if(str.length===2)
+  {
+    return str;
+  }
+  else {
+    let len=str.length-1;
+   return str.slice(1, len);
+  }
+}
